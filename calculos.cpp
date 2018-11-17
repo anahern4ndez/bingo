31c93428b38a00d5d364237e3a3f54bcbb6da3e0
her17138 <her17138@uvg.edu.gt>
2018-11-16 23:21:16 -0600

    cambio cpp

--- calculos.cpp
+++ calculos.cpp
@@ -62,8 +62,11 @@ __global__ void prediccion(float *y, float *x, double m, double b)//y es el vect
 {
     y[(int)threadIdx.x] = ((float)m*x[(int)threadIdx.x]) + (float)b; //ecuacion de una recta
 }
+
+
 int main(int argv, char* argc[])
 {
+	printf("Iniciando");
     /* creacion streams */
     cudaStream_t stream1, stream2, stream3;
     cudaStreamCreate(&stream1);
@@ -91,28 +94,42 @@ int main(int argv, char* argc[])
     //stream 3
     cudaMalloc( (void**)&dev_pres, N * sizeof(int) );
     cudaHostAlloc( (void**)&pres, N * sizeof(int), cudaHostAllocDefault);
+
+printf("Streams terminados");
     
     /* lectura de datos del csv */
     int i = 0; //indice
     string humedad, presion, temperatura, fecha;
     ifstream file("datos.csv");
+printf("Leyendo CSV");
     while (getline(file, humedad, ',')) {
+		printf("\nIM COMING IN\n");
         std::size_t offset = 0;
         hum[i] = stod(humedad,&offset);
+		printf("STOD1:%.2f\n", hum[i]);
         offset = 0;
         getline(file, presion, ',') ;
         pres[i] = stod(presion,&offset);
+		printf("STOD2:%.2f\n", pres[i]);
         offset = 0;
         getline(file, temperatura, ',') ;
         temp[i] = stod(temperatura,&offset);
+		printf("STOD3:%.2f\n", temp[i]);
         getline(file, fecha);
         fechas[i] = fecha;
+		printf("Fecha: %s\n", fechas[i].c_str());
+		printf("%d\n",i);
         i++;
     }
+
+printf("CSV leido");
+
     for (int i =0; i <N; i++)
     {
         secs[i] = 900*i;
     }
+
+printf("Calculando regresion");
     /* calculo de la regresion lineal para temperatura, presion y humedad */
     regresionLineal regHum = calculoRegresion(hum, secs);
     regresionLineal regPres = calculoRegresion(pres, secs);
@@ -120,20 +137,35 @@ int main(int argv, char* argc[])
     
     /* lanzamiento de kernels para la prediccion
         se lanzaran N threads en los que cada uno calculara la prediccion en la hora correspondiente de su variable correspondiente. */
-    cudaMemcpyAsync(dev_hum,hum,N*sizeof(int),cudaMemcpyHostToDevice,stream1);
+
+   printf("Pasando a Device");
+ cudaMemcpyAsync(dev_hum,hum,N*sizeof(int),cudaMemcpyHostToDevice,stream1);
     cudaMemcpyAsync(dev_pres,pres,N*sizeof(int),cudaMemcpyHostToDevice,stream2);
     cudaMemcpyAsync(dev_temp,temp,N*sizeof(int),cudaMemcpyHostToDevice,stream3);
     cudaMemcpyAsync(dev_secs,secs,N*sizeof(int),cudaMemcpyHostToDevice,stream1);
     cudaMemcpyAsync(dev_secs,secs,N*sizeof(int),cudaMemcpyHostToDevice,stream2);
     cudaMemcpyAsync(dev_secs,secs,N*sizeof(int),cudaMemcpyHostToDevice,stream3);
+
+printf("Kernels");
+
     prediccion<<<1, N, 0, stream1>>>(dev_hum, dev_secs, regHum.pendiente, regHum.intercepto);
     prediccion<<<1, N, 1, stream2>>>(dev_pres, dev_secs, regPres.pendiente, regPres.intercepto);
     prediccion<<<1, N, 2, stream3>>>(dev_temp, dev_secs, regTemp.pendiente, regTemp.intercepto);
-    cudaMemcpyAsync(hum,dev_hum,N*sizeof(int),cudaMemcpyDeviceToHost,stream1);
+
+printf("Device a Host");
+
+cudaMemcpyAsync(hum,dev_hum,N*sizeof(int),cudaMemcpyDeviceToHost,stream1);
     cudaMemcpyAsync(pres,dev_pres,N*sizeof(int),cudaMemcpyDeviceToHost,stream2);
     cudaMemcpyAsync(temp,dev_temp,N*sizeof(int),cudaMemcpyDeviceToHost,stream3);
     
-    
+printf("Prints");
+
+for(int i=0; i<N; i++)
+{
+	printf("Humedad dia %d: %.2f\n", i, hum[i]);
+	printf("Presion dia %d: %.2f\n", i, pres[i]);
+	printf("Temperatura dia %d: %.2f\n", i, temp[i]);
+}
     /* display de prediccion o escritura en un nuevo .csv */
     
     
